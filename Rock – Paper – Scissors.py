f5f1f0dbf951c5d3022e334b1978ec3244f922a3
Nikola Georgiev <nik123xyz@gmail.com>
2023-10-07 13:22:46 +0100

    Update - Scoring , Quit Option

--- Rock – Paper – Scissors.py
+++ Rock – Paper – Scissors.py
@@ -1,50 +1,62 @@
 import random
 
-rock = "Rock"
-paper = "Paper"
-scissors = "Scissors"
+choices = {
+    'r': "Rock",
+    'p': "Paper",
+    's': "Scissors",
+}
 
 RED = "\033[91m"
 GREEN = "\033[92m"
 YELLOW = "\033[93m"
+RESET = "\033[0m"
 
-player_move = input("Choose [r]ock, [p]aper, [s]cissors: ")
+user_score = 0
+computer_score = 0
+rounds_played = 0
 
-if player_move == "r":
-    player_move = rock
+def determine_winner(player_choice, computer_choice):
+    global user_score, computer_score, rounds_played
+    if player_choice == computer_choice:
+        return "Draw"
+    elif (
+        (player_choice == 'r' and computer_choice == 's') or
+        (player_choice == 'p' and computer_choice == 'r') or
+        (player_choice == 's' and computer_choice == 'p')
+    ):
+        user_score += 1
+        return "User"
+    else:
+        computer_score += 1
+        return "Computer"
 
-elif player_move == "p":
-    player_move = paper
+while True:
+    print(f"Round {rounds_played + 1}")
+    print(f"User Score: {user_score}  Computer Score: {computer_score}")
 
-elif player_move == "s":
-    player_move = scissors
+    # User input
+    while True:
+        user_move = input("Choose [r]ock, [p]aper, or [s]cissors (or 'q' to quit): ").lower()
+        if user_move == 'q':
+            print("Thanks for playing!")
+            exit()
+        if user_move in choices:
+            break
+        else:
+            print("Invalid input. Try again.")
 
-else:
-    raise SystemExit("Invalid Input. Try again...")
+    player_move = choices[user_move]
+    
+    computer_move = choices[random.choice(list(choices.keys()))]
+    print(f"The computer chose {computer_move}.")
 
-computer_random_number = random.randint(1, 3)
+    result = determine_winner(user_move, random.choice(list(choices.keys())))
 
-computer_move = ""
+    if result == "Draw":
+        print(YELLOW + "It's a draw!" + RESET)
+    elif result == "User":
+        print(GREEN + f"You win! {player_move} beats {computer_move}" + RESET)
+    else:
+        print(RED + f"You lose! {computer_move} beats {player_move}" + RESET)
 
-if computer_random_number == 1:
-    computer_move = rock
-
-elif computer_random_number == 2:
-    computer_move = paper
-
-else:
-    computer_move = scissors
-
-print(f"The computer chose {computer_move}.")
-
-
-if (player_move == rock and computer_move == scissors) or \
-    (player_move == paper and computer_move == rock) or \
-    (player_move == scissors and computer_move == paper):
-    print(GREEN + "You win!")
-
-elif player_move == computer_move:
-    print(YELLOW + "Draw!")
-
-else:
-    print(RED + "You lose!")
+    rounds_played += 1
